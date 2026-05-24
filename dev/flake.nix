dc0c46366354ec96b649ec32726b351b24024c35
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-05-24 21:05:40 +0900

    fix: ルートのflake.nixにtreefmtを定義

--- dev/flake.nix
+++ dev/flake.nix
@@ -5,18 +5,11 @@
     root.url = "path:../";
     nixpkgs.follows = "root/nixpkgs";
     flake-parts.follows = "root/flake-parts";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -24,21 +17,13 @@
         "x86_64-darwin"
       ];
       perSystem =
-        { config, pkgs, ... }:
+        { inputs', pkgs, ... }:
         {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs.nixfmt = {
-              enable = true;
-              package = pkgs.nixfmt;
-            };
-          };
-
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               statix
               nixd
-              config.treefmt.build.wrapper
+              inputs'.root.formatter
             ];
             shellHook = ''
               export REPO_ROOT=$(git rev-parse --show-superproject-working-tree --show-toplevel)
--- flake.nix
+++ flake.nix
@@ -4,17 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt = {
+              enable = true;
+              package = pkgs.nixfmt;
+            };
+          };
+        };
       flake = {
         lib = import ./lib;
         homeManagerModules.default = ./modules/home-manager.nix;
